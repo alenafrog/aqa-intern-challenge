2c4cb9da46df66e01e5f6ba47e19edb6b73db0ab
Alena R <aleona.romanova2015@yandex.ru>
2025-04-13 00:45:42 +0700

    Fix btn write us locator

--- tests/widget.page.ts
+++ tests/widget.page.ts
@@ -5,7 +5,7 @@ enum WidgetPageSelectors {
     WIDGET_BODY = '[class^=widgetWrapper] > [class^=widget__]',
     HEADER_TEXT = 'header h5',
     BUTTON_OPEN = '[data-test=openWidget]',
-    BUTTON_WRITE_TO_US = '[class^=btn]',
+    BUTTON_WRITE_TO_US = 'span[class^=questions__] + [class^=btn]',
     ARTICLE_POPULAR_TITLE = '[class^=popularTitle__]',
     ARTICLE_POPULAR_LIST = `${ARTICLE_POPULAR_TITLE} + ul[class^=articles__]`,
     ARTICLE_POPULAR_LIST_ITEM = `${ARTICLE_POPULAR_LIST} > li`,
@@ -17,7 +17,7 @@ export class WidgetPage {
     constructor(protected page: Page) {}
 
     wrapper() {
-        return this.page.locator(WidgetPage.selector.WRAPPER)
+        return this.page.locator(WidgetPage.selector.WRAPPER);
     }
 
     async openWidget() {
