6202832ccebfd4d783a0625bdc0f867f7e565af8
Alena R <aleona.romanova2015@yandex.ru>
2025-04-13 18:15:03 +0700

    New test - check all articles categories

--- tests/widget.page.ts
+++ tests/widget.page.ts
@@ -1,4 +1,4 @@
-import {Page} from "@playwright/test";
+import {Locator, Page} from "@playwright/test";
 
 enum WidgetPageSelectors {
     WRAPPER = '.sc-dino-typography-h > [class^=widget__]',
@@ -9,6 +9,10 @@ enum WidgetPageSelectors {
     ARTICLE_POPULAR_TITLE = '[class^=popularTitle__]',
     ARTICLE_POPULAR_LIST = `${ARTICLE_POPULAR_TITLE} + ul[class^=articles__]`,
     ARTICLE_POPULAR_LIST_ITEM = `${ARTICLE_POPULAR_LIST} > li`,
+    BUTTON_ALL_ARTICLES = '[data-test=button_all_articles]',
+    ALL_ARTICLES_SECTION = '[class^=section__]',
+    ALL_ARTICLES_SECTION_TITLE = '[class^=section-title__]',
+    ALL_ARTICLES_SECTION_CATEGORY_TITLE = '[class^=category__] div[class^=title__]',
 }
 
 export class WidgetPage {
@@ -27,17 +31,35 @@ export class WidgetPage {
     async getPopularArticles() {
         return this.wrapper().locator(WidgetPage.selector.ARTICLE_POPULAR_LIST_ITEM);
     }
+    
+    async getAllArticlesSections() {
+        return this.wrapper().locator(WidgetPage.selector.ALL_ARTICLES_SECTION);
+    }
+
 
     async clickWriteToUs() {
         return this.wrapper().locator(WidgetPage.selector.BUTTON_WRITE_TO_US).click();
+    }
+
+    async clickButtoAllArticles() {
+        return this.wrapper().locator(WidgetPage.selector.BUTTON_ALL_ARTICLES).click();
     }
 
     async getTitle() {
         return this.wrapper().locator(WidgetPage.selector.HEADER_TEXT).textContent();
     }
 
+    async findSectionTitle(section: Locator) {
+        return section.locator(WidgetPage.selector.ALL_ARTICLES_SECTION_TITLE).textContent();
+    }
+
+    async findSectionCategoriesTitles(section: Locator) {
+        return section.locator(WidgetPage.selector.ALL_ARTICLES_SECTION_CATEGORY_TITLE);
+    }
+
     getWidgetBody() {
         return this.page.locator(WidgetPage.selector.WIDGET_BODY);
     }
+
 }
 
--- tests/widget.spec.ts
+++ tests/widget.spec.ts
@@ -22,8 +22,6 @@ test.describe('Uchi.ru widget ', () => {
 
   test('has correct title', async ({ page }) => {
     await widgetPage.openWidget();
-	
-	  await expect(widgetPage.getWidgetBody()).toBeVisible();
 
     const articles = await widgetPage.getPopularArticles();
 	  await expect(articles).toHaveCount(5);
@@ -34,4 +32,24 @@ test.describe('Uchi.ru widget ', () => {
 
     expect(await widgetPage.getTitle()).toEqual('Связь с поддержкой');
   });
+
+  test('check articles sections fillness', async ({ page }) => {
+    await widgetPage.openWidget();
+
+    await widgetPage.clickButtoAllArticles();
+
+    const sections = await widgetPage.getAllArticlesSections();
+    await expect(sections).toHaveCount(3);
+
+    for (const section of await sections.all()) {
+      const sectionTitle = await widgetPage.findSectionTitle(section);
+      //console.log(sectionTitle);
+      expect(sectionTitle).toBeDefined();
+
+      const categoryTitles = await widgetPage.findSectionCategoriesTitles(section);
+      //console.log(await categoryTitles.count());
+      expect(await categoryTitles.count()).toBeGreaterThan(0);
+    }
+  }); 
+
 });
